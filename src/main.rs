ba0f740dee0a5a7a6603b17a31fdf809640a08a5
Viktor Ovsiichuk <victor.ovseychuk@gmail.com>
2022-02-07 03:32:40 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ const WINDOW_HEIGHT_PX: u32 = 800;
 const GRID_WIDTH_CELLS: usize = 100;
 const GRID_HEIGHT_CELLS: usize = 100;
 
-const CELL_WIDTH_PX: f32 = WINDOW_WIDTH_PX as f32 / GRID_WIDTH_CELLS as f32;
-const CELL_HEIGHT_PX: f32 = WINDOW_HEIGHT_PX as f32 / GRID_HEIGHT_CELLS as f32;
-
 const MOUSE_SPAWN_RADIUS: u8 = 1;
 
 const BACKGROUND: Rgb8 = rgb8(255, 237, 219);
@@ -32,8 +29,6 @@ const COLORS: [Rgb8; 4] = [
 
 const DRAW_FPS: bool = true;
 const REDRAW_FPS_FRAMES: u64 = 8;
-
-struct RowColumnIndex(usize, usize);
 
 fn main() {
     nannou::app(model)
@@ -56,7 +51,10 @@ fn model(app: &App) -> Model {
         fps: 0.0,
         counter: 0,
         brush: Brush { fill: 1, active: false, radius: MOUSE_SPAWN_RADIUS },
-        grid: Grid::new(GRID_WIDTH_CELLS, GRID_HEIGHT_CELLS, 0),
+        simulation: Simulation {
+            grid: Grid::new(GRID_WIDTH_CELLS, GRID_HEIGHT_CELLS, 0),
+            bounds: app.window_rect(),
+        },
     }
 }
 
@@ -81,14 +79,13 @@ fn event(_app: &App, model: &mut Model, event: WindowEvent) {
 fn update(app: &App, model: &mut Model, update: Update) {
     model.fps = 1000.0 / update.since_last.as_millis() as f64;
 
-    model.grid.prepare();
+    model.simulation.prepare();
 
     if model.brush.active {
-        let grid_coords = convert_global_to_grid_coords(app.mouse.position());
-        model.grid.spawn(grid_coords.0, grid_coords.1, model.brush.radius, model.brush.fill);
+        model.simulation.spawn(app.mouse.position(), model.brush.radius, model.brush.fill);
     }
 
-    model.grid.step();
+    model.simulation.step();
 
     model.counter += 1;
 }
@@ -96,7 +93,7 @@ fn update(app: &App, model: &mut Model, update: Update) {
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
-    model.grid.display(&draw);
+    model.simulation.display(&draw);
 
     if DRAW_FPS && frame.nth() % REDRAW_FPS_FRAMES == 0 {
         draw_fps(app, &draw, model);
@@ -109,7 +106,7 @@ struct Model {
     fps: f64,
     counter: u64,
     brush: Brush,
-    grid: Grid,
+    simulation: Simulation,
 }
 
 type CellValue = u8;
@@ -124,6 +121,74 @@ struct Brush {
 struct Cell {
     value: CellValue,
     updated: bool,
+}
+
+struct Simulation {
+    grid: Grid,
+    bounds: Rect,
+}
+
+impl Simulation {
+
+    fn prepare(&mut self) {
+        self.grid.prepare();
+    }
+
+    fn step(&mut self) {
+        self.grid.step();
+    }
+
+    fn display(&self, draw: &Draw) {
+        let cell_width = self.bounds.w() / self.grid.width as f32;
+        let cell_height = self.bounds.h() / self.grid.height as f32;
+
+        let start_x = self.bounds.left() + cell_width / 2.0;
+        let start_y = self.bounds.top() - cell_height / 2.0;
+
+        for row in 0..self.grid.height {
+            let row_y = start_y - row as f32 * cell_height;
+
+            for column in 0..self.grid.width {
+                let cell = self.grid.get(row, column);
+
+                if cell.updated {
+                    let cell_x = start_x + column as f32 * cell_width;
+                    Simulation::draw_cell(draw, cell_x, row_y, cell_width, cell_width, cell.value);
+                }
+            }
+        }
+    }
+
+    fn draw_cell(draw: &Draw, x: f32, y: f32, w: f32, h: f32, value: CellValue) {
+        let color = COLORS[value as usize];
+        draw.rect()
+            .x_y(x, y)
+            .w_h(w, h)
+            .color(color);
+    }
+
+    fn spawn(&mut self, mouse: Vec2, radius: u8, fill: CellValue) {
+        let r = radius as usize;
+        let cell_width = self.bounds.w() / self.grid.width as f32;
+        let cell_height = self.bounds.h() / self.grid.height as f32;
+
+        let row = ((self.bounds.y() + self.bounds.h() / 2.0 - mouse.y) / cell_height) as usize;
+        let column = ((self.bounds.x() + self.bounds.w() / 2.0 + mouse.x) / cell_width) as usize;
+
+        let brush_row_from = row.saturating_sub(r);
+        let brush_row_to = min(row + r, self.grid.height - 1);
+
+        let brush_col_from = column.saturating_sub(r);
+        let brush_col_to = min(column + r, self.grid.width - 1);
+
+        for i in brush_row_from..=brush_row_to {
+            for j in brush_col_from..=brush_col_to {
+                if self.grid.get(i, j).value == 0 || fill == 0 {
+                    self.grid.set(i, j, fill);
+                }
+            }
+        }
+    }
 }
 
 struct Grid {
@@ -148,6 +213,7 @@ impl Debug for Grid {
 }
 
 impl Grid {
+
     fn new(width: usize, height: usize, fill: CellValue) -> Self {
         let buffer = vec![Cell { value: fill, updated: false }; width * height];
         let rng = rand::thread_rng();
@@ -194,9 +260,10 @@ impl Grid {
                         }
                         [true, _, true] => {
                             let go_left: bool = self.rng.gen();
-                            let dx = (go_left as usize) * 2 - 1; // map 0 1 to -1 1
+                            let dx = (go_left as i32) * 2 - 1; // map 0 1 to -1 1
+                            let new_column = (column as i32 + dx) as usize;
                             self.set(row, column, 0);
-                            self.set(row + 1, column + dx, current_value);
+                            self.set(row + 1, new_column, current_value);
                         }
                         [true, _, _] => {
                             self.set(row, column, 0);
@@ -212,58 +279,6 @@ impl Grid {
             }
         }
     }
-
-    fn display(&self, draw: &Draw) {
-        let start_x = -(WINDOW_WIDTH_PX as f32) / 2.0 + CELL_WIDTH_PX / 2.0;
-        let start_y = WINDOW_HEIGHT_PX as f32 / 2.0 - CELL_HEIGHT_PX / 2.0;
-
-        for row in 0..self.height {
-            let row_y = start_y - row as f32 * CELL_HEIGHT_PX;
-
-            for column in 0..self.width {
-                let cell = self.get(row, column);
-
-                if cell.updated {
-                    let cell_x = start_x + column as f32 * CELL_WIDTH_PX;
-                    Grid::draw_cell(draw, cell_x, row_y, CELL_WIDTH_PX, CELL_HEIGHT_PX, cell.value);
-                }
-            }
-        }
-    }
-
-    fn draw_cell(draw: &Draw, x: f32, y: f32, w: f32, h: f32, value: u8) {
-        let color = COLORS[value as usize];
-        draw.rect()
-            .x_y(x, y)
-            .w_h(w, h)
-            .color(color);
-    }
-
-    fn spawn(&mut self, row: usize, column: usize, radius: u8, fill: CellValue) {
-        let r = radius as usize;
-
-        let brush_row_from = row.saturating_sub(r);
-        let brush_row_to = min(row + r, GRID_HEIGHT_CELLS - 1);
-
-        let brush_col_from = column.saturating_sub(r);
-        let brush_col_to = min(column + r, GRID_WIDTH_CELLS - 1);
-
-        for i in brush_row_from..=brush_row_to {
-            for j in brush_col_from..=brush_col_to {
-                if self.get(i, j).value == 0 || fill == 0 {
-                    self.set(i, j, fill);
-                }
-            }
-        }
-    }
-}
-
-fn convert_global_to_grid_coords(vec: Vec2) -> RowColumnIndex {
-    let row = (WINDOW_HEIGHT_PX as f32 / 2.0 - vec.y) / CELL_HEIGHT_PX;
-    let column = (vec.x + WINDOW_WIDTH_PX as f32 / 2.0) / CELL_WIDTH_PX;
-
-    RowColumnIndex(clamp(row as usize, 0, GRID_HEIGHT_CELLS - 1),
-                   clamp(column as usize, 0, GRID_WIDTH_CELLS - 1))
 }
 
 fn draw_fps(app: &App, draw: &Draw, model: &Model) {
